abfb78a6230d18883e677e13a7d93d685bfd01fa
shivrjpatil <shivrajp972@gmail.com>
2023-01-10 16:40:08 +0530

    Created Queue and Enqueue elements

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -11,12 +11,13 @@ namespace StackandQueue
         static void Main(string[] args)
         {
             Stacks stack = new Stacks();
+            Queue queue = new Queue();
             Console.WriteLine("Welcome to the Stacks And Queues Operations");
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("-----------------------------------------");
-                Console.WriteLine("1.Create Stack\n2.PeekAndPop\n3.Exit");
+                Console.WriteLine("1.Create Stack\n2.PeekAndPop\n3.Create Queue\n4.Exit");
                 Console.Write("Enter your choice: ");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
@@ -34,6 +35,12 @@ namespace StackandQueue
                         stack.IsEmpty();
                         break;
                     case 3:
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        queue.Display();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
---
+++ StackandQueue/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    internal class Queue
+    {
+        Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted into Queue", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            Console.WriteLine("Queue is as:");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
